2b019a586217c8fafbbf81207541d1fd49b21693
Noah <noah.rousseau2412@gmail.com>
2025-05-06 00:08:30 +0200

    quelques ajustements

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"),  // Définit "client" comme racine du projet Vite
+  root: path.resolve(__dirname, "starfall/client"),  // Définit "client" comme racine du projet Vite
   server: {
-    open: true,
+    open: false,
     port: 22221,
     host: true,
   },
   build: {
-    outDir: path.resolve(__dirname, "client/dist"), // Génère les fichiers buildés ici
+    outDir: path.resolve(__dirname, "starfall/client/dist"), // Génère les fichiers buildés ici
   },
   optimizeDeps: {
     include: ["@babylonjs/core", "@babylonjs/loaders"],
